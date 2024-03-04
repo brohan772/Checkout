4d81a51d0ed91cd265302ec744ed058f44117018
Rohan Bhalla <brohan772@gmail.com>
2024-03-04 02:22:15 +0000

    Implemented solution to pass ScanItemsTest

--- Shopping-Checkout/Checkout.cs
+++ Shopping-Checkout/Checkout.cs
@@ -8,9 +8,12 @@ namespace Shopping_Checkout
     public class Checkout : ICheckout
     {
         private List<ItemPrices> _itemPrices;
+        private Dictionary<string, int> _scannedItems;
+
         public Checkout()
         {
             _itemPrices = new List<ItemPrices>();
+            _scannedItems = new Dictionary<string, int>();
         }
 
         public void SetItemPrices(List<ItemPrices> itemPrices)
@@ -26,11 +29,34 @@ namespace Shopping_Checkout
 
         public void Scan(string item)
         {
+            //if scanned item has already been scanned, increment it's quantity by 1, if not then add the item to the dict
+            if (_scannedItems.ContainsKey(item))
+            {
+                _scannedItems[item] += 1;
+            }
+            else
+            {
+                _scannedItems.Add(item, 1);
+            }
         }
 
         public int GetTotalPrice()
         {
-            return 0;
+            int totalPrice = 0;
+
+            foreach (var item in _scannedItems)
+            {
+                var sku = item.Key;
+                var quantity = item.Value;
+
+                var itemPrice = _itemPrices.FirstOrDefault(i => i.SKU == sku);
+
+                if (itemPrice != null)
+                {
+                    totalPrice += itemPrice.UnitPrice * quantity;
+                }
+            }
+            return totalPrice;
         }
     }
 }
