51252fe9f8114ff781bd1fb68d0e4bde3bdba7d0
Rohan Bhalla <brohan772@gmail.com>
2024-03-04 02:10:08 +0000

    Failing test to scan items and get total price

--- Shopping-Checkout.tests/CheckoutTests.cs
+++ Shopping-Checkout.tests/CheckoutTests.cs
@@ -22,7 +22,23 @@ namespace Shopping_Checkout.Tests
             Assert.AreEqual(itemPrices, checkout.GetItemPrices());
         }
 
+        [Test]
+        public void ScanItemsTest()
+        {
+            var checkout = new Checkout();
 
+            var itemPrices = new List<ItemPrices>
+            {
+                new ItemPrices("A", 50),
+                new ItemPrices("B", 40)
+            };
+
+            checkout.SetItemPrices(itemPrices);
+            checkout.Scan("A");
+            checkout.Scan("B");
+
+            Assert.AreEqual(90, checkout.GetTotalPrice());
+        }
 
     }
 }
