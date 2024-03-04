c07b83423e82b7093939556821163a01016c0ff0
Rohan Bhalla <brohan772@gmail.com>
2024-03-04 02:06:25 +0000

    Implemented solution to pass SetItemPricesTest

---
+++ Shopping-Checkout/Checkout.cs
@@ -0,0 +1,36 @@
+﻿using Shopping_Checkout;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Shopping_Checkout
+{
+    public class Checkout : ICheckout
+    {
+        private List<ItemPrices> _itemPrices;
+        public Checkout()
+        {
+            _itemPrices = new List<ItemPrices>();
+        }
+
+        public void SetItemPrices(List<ItemPrices> itemPrices)
+        {
+            _itemPrices = itemPrices;
+        }
+
+        public List<ItemPrices> GetItemPrices()
+        {
+            return _itemPrices;
+        }
+
+
+        public void Scan(string item)
+        {
+        }
+
+        public int GetTotalPrice()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Shopping-Checkout/ItemPrices.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Shopping_Checkout
+{
+    public class ItemPrices
+    {
+        public string SKU { get; set; }
+        public int UnitPrice { get; set; }
+
+        public ItemPrices(string sku, int unitPrice)
+        {
+            SKU = sku;
+            UnitPrice = unitPrice;
+        }
+    }
+}
